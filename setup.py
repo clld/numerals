82fa3ff316cb0f3abc1ced23a30a0e4fb1dfe389
Christoph Rzymski <christoph@functor-argument.net>
2018-01-30 14:07:19 +0100

    Proposal for updating scaffolding/boilerplate files. (#1)
    * Proposal for updating scaffolding/boilerplate files. Ideally, this could
    serve as a template for the other apps.
    
    * Proposal for updating scaffolding/boilerplate files. Ideally, this could
    serve as a template for the other apps.

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name='numerals',
     version='0.0',
@@ -19,14 +18,21 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'clld', 'clldmpg>=3.1.1',
+        'clld~=4.0',
+        'clldmpg>=3.1.1',
+        'sqlalchemy',
+        'waitress',
     ],
     extras_require={
-        'dev': ['flake8', 'waitress'],
+        'dev': [
+            'flake8',
+            'waitress',
+            'tox',
+        ],
         'test': [
             'mock',
             'pytest>=3.1',
-            'pytest-clld',
+            'pytest-clld>=0.4',
             'pytest-mock',
             'pytest-cov',
             'coverage>=4.2',
