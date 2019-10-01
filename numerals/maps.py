d56c0172444e0311ad9d9bc66b15b75535677418
Hans-Jörg Bibiko <bibiko@shh.mpg.de>
2019-10-01 11:19:12 +0200

    customized maps

--- numerals/maps.py
+++ numerals/maps.py
@@ -1,2 +1,22 @@
+from clld.web.maps import ParameterMap, Map
+
+
+class NumeralParameterMap(ParameterMap):
+    def get_options(self):
+        return {
+            'base_layer': 'Esri.WorldPhysical',
+            'icon_size': 15,
+            'max_zoom': 9,
+        }
+
+class LanguagesMap(Map):
+    def get_options(self):
+        return {
+            'base_layer': 'Esri.WorldPhysical',
+            'icon_size': 15,
+            'max_zoom': 9,
+        }
+
 def includeme(config):
-    pass
+    config.register_map('parameter', NumeralParameterMap)
+    config.register_map('languages', LanguagesMap)
